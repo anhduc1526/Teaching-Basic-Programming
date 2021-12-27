df1e8c3a1d6ccebdbe2fa725be4ec59a0e79bc88
Eskimo111 <62143480+Eskimo111@users.noreply.github.com>
2021-12-27 09:38:36 +0700

    Add files via upload

---
+++ Graph/Code/BFS.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int N = 1e5 + 7;
+
+int n, m;
+vector <int> G[N];
+bool visit[N];
+queue <int> q;
+
+void BFS(int i)
+{
+    q.push(i);
+    while(!q.empty())
+    {
+        int u = q.front();
+        q.pop();
+        //if(!visit[u]) cout << u << " ";
+        visit[u] = true;
+
+        for(int v : G[u])
+            if(!visit[v])
+        {
+            q.push(v);
+        }
+    }
+}
+
+int main()
+{
+    cin >> n >> m;
+    for(int i = 1 ; i <= m ; i++)
+    {
+        int u, v;
+        cin >> u >> v;
+        G[u].push_back(v);
+        G[v].push_back(u);
+    }
+
+    for(int i = 1 ; i <= n ; i++)
+        if(!visit[i]) BFS(i);
+    //BFS(3);
+}
